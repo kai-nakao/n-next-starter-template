a5a48887def867437bede43c92d1bbcbf28b5a10
kai-nakao <nkkai0803@gmail.com>
2023-12-19 08:56:12 +0900

    add: temp header ui

---
+++ src/features/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className=" flex bg-gray-500 h-14 px-4 items-center">
+      <h1>Group</h1>
+    </header>
+  );
+};
+
+export default Header;
