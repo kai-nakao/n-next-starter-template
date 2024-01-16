10c7c79607d34bcfa7a9363e854540beb640f6a6
kai-nakao <nkkai0803@gmail.com>
2024-01-17 08:34:54 +0900

    feat: get cityList

--- src/features/Main/RootPage/CitiesSection/CheckBox.tsx
+++ src/features/Main/RootPage/CitiesSection/CheckBox.tsx
@@ -7,7 +7,7 @@ export const CheckBox = ({ value, cityName }: CheckBoxProps) => {
   return (
     <div className="flex gap-1">
       <input type="checkbox" id={value} name="city" value={value} />
-      <label htmlFor="tamano">{`${cityName}市`}</label>
+      <label htmlFor={value}>{cityName}</label>
     </div>
   )
 }
--- src/features/Main/RootPage/CitiesSection/index.tsx
+++ src/features/Main/RootPage/CitiesSection/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 
+import { useCityList } from '@/services/city'
+
 import { CheckBox } from './CheckBox'
 
 export const CitiesSection = () => {
+  const { data } = useCityList({})
   return (
     <fieldset className="flex flex-col items-center justify-center">
       {/* legend html  differs css style depend on browser */}
       <div>
         <legend>Choose your interests</legend>
       </div>
-      <section className="m-10 flex gap-4">
-        <CheckBox value="tamano" cityName="玉野" />
-        <CheckBox value="okayama" cityName="岡山" />
-        <CheckBox value="kurasiki" cityName="倉敷" />
+      <section className="m-10 grid grid-cols-6 gap-4">
+        {data &&
+          data.map((city: City, index: number) => (
+            <CheckBox
+              key={index}
+              value={city.cityCode}
+              cityName={city.cityName}
+            />
+          ))}
       </section>
     </fieldset>
   )
