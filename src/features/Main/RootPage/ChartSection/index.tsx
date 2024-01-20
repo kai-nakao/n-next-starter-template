3d005cb2f0790d565f720e4582b674ff627878c1
kai-nakao <nkkai0803@gmail.com>
2024-01-20 15:06:03 +0900

    fix: get flexible city name

--- src/features/Main/RootPage/ChartSection/index.tsx
+++ src/features/Main/RootPage/ChartSection/index.tsx
@@ -1,12 +1,24 @@
 import { Card, Title, LineChart } from '@tremor/react'
+import { useAtom } from 'jotai'
 
+import { useCityList } from '@/services/city'
 import { useChartData } from '@/services/population'
+
+import { selectedCityAtom } from '../CitiesSection/atoms/selectedCity'
 
 export const ChartSection = () => {
   const { data } = useChartData({})
+  const { data: cityList } = useCityList({})
+
+  const [selectedCity] = useAtom(selectedCityAtom)
+
+  // get city name from city code(atom)
+  const cityName =
+    cityList?.find((city) => city.cityCode === selectedCity)?.cityName ||
+    '未選択'
   return (
     <Card className="w-10/12">
-      <Title>玉野市の人口増減グラフ</Title>
+      <Title>{`${cityName}の人口増減グラフ`}</Title>
       <LineChart
         className="h-96"
         data={data}
