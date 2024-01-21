df12d1340c0d12fc591905108568fecde102405a
kai-nakao <nkkai0803@gmail.com>
2024-01-22 08:19:48 +0900

    remove: filter specific city

--- src/services/city/selector.ts
+++ src/services/city/selector.ts
@@ -1,9 +1,5 @@
 export const getCitySelector = (data: any) => {
   const cityData = data?.result
 
-  const filteredCityData: City[] = cityData.filter((city: City) => {
-    return !city.cityName.startsWith('岡山市') || !city.cityName.endsWith('区')
-  })
-
-  return filteredCityData
+  return cityData
 }
