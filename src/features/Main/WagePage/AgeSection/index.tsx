e0168a865aba267ac3c8cdf71fac64a980d36094
kai-nakao <nkkai0803@gmail.com>
2024-01-23 09:02:38 +0900

    feat: age section ui

---
+++ src/features/Main/WagePage/AgeSection/index.tsx
@@ -0,0 +1,29 @@
+import { useAtom } from 'jotai'
+
+import { selectedAgeAtom } from './atoms/selectedAge'
+import { AgeRanges } from './const'
+import { CheckBox } from '../../SharedComponents/CheckBox'
+
+export const AgeSection = () => {
+  const [selectedAge, setSelectedAge] = useAtom(selectedAgeAtom)
+
+  return (
+    <fieldset className="flex flex-col items-center justify-center">
+      {/* legend html  differs css style depend on browser */}
+      <div>
+        <legend>年齢</legend>
+      </div>
+      <section className="m-10 grid grid-cols-3 gap-4">
+        {Object.entries(AgeRanges).map(([key, value]) => (
+          <CheckBox
+            key={key}
+            value={value}
+            name={value}
+            selected={selectedAge === Number(key)}
+            onSelect={() => setSelectedAge(Number(key))}
+          />
+        ))}
+      </section>
+    </fieldset>
+  )
+}
