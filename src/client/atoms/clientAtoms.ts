725c8a400ee2c651704a9fdf1f47dd0520a28c81
kai-nakao <nkkai0803@gmail.com>
2024-02-14 17:01:27 +0900

    fix: config baseURL

--- src/client/atoms/clientAtoms.ts
+++ src/client/atoms/clientAtoms.ts
@@ -6,11 +6,21 @@ import { trpc } from '@/utils/trpc'
 
 export const queryClientAtom = atom(new QueryClient())
 
+function getBaseUrl() {
+  if (typeof window !== 'undefined')
+    // browser should use relative path
+    return ''
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`
+}
 export const trpcClientAtom = atom(() =>
   trpc.createClient({
     links: [
       httpBatchLink({
-        url: 'http://localhost:3000/api/trpc',
+        url: `${getBaseUrl()}/api/trpc`,
       }),
     ],
   }),
